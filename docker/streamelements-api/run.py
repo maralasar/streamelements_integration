7a7a11c6f8279d3d61286204ad9770d7bb9e626a
maralasar <balasar@balasar.de>
2024-11-05 00:48:49 +0100

    Fix a small bug that caused the requests to outrun the present time and request future time windows

--- docker/streamelements-api/run.py
+++ docker/streamelements-api/run.py
@@ -32,12 +32,16 @@ def main():
     throttle_time = int(os.environ.get("SE_API_THROTTLE", 5))
     with StreamelementsAPIIngester(guid=guid) as client:
         while True:
-            last_iteration = dt.datetime.now(tz=dt.timezone.utc)
-            last_date = client.retrieve_data(after) - dt.timedelta(seconds=1e-5)
+            data_last_iter = dt.datetime.now(tz=dt.timezone.utc)
+            date_last_datapoint = client.retrieve_data(after) - dt.timedelta(seconds=1e-5)
             _logger.debug(f"Waiting {throttle_time} seconds before next iteration")
             time.sleep(throttle_time)
-            after = last_date
-            if last_date >= last_iteration - dt.timedelta(seconds=sleep_time):
+
+            if date_last_datapoint >= dt.datetime.now(tz=dt.timezone.utc):
+                _logger.info("Last request window extended into the future. Choose SE_API_SLEEP larger than SE_API_REQUEST_WINDOW")
+            after = min(date_last_datapoint, data_last_iter)
+
+            if after >= data_last_iter - dt.timedelta(seconds=sleep_time):
                 _logger.info(f"Last requested window ended at {after.isoformat()}. Waiting {sleep_time} seconds")
                 time.sleep(sleep_time)
 
